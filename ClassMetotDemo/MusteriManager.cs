c304384912a416a3b5df6fd48530fa75a5ebe27a
IlaydaErdem <71014982+IlaydaErdem@users.noreply.github.com>
2021-02-09 00:08:42 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Adınıza Hesabınız açıldı :" + musteri.Adi + " " + musteri.Soyadi);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Hesabınız silindi :" + musteri.Adi + " " + musteri.Soyadi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir bankada müşteri takibi yapmak istiyorsunuz.--Musteri isimli bir Class oluşturunuz. Müşteriye istediğiniz özellikleri ekleyiniz. (Id,Ad,Soyad....)
+            //MusteriManager sınıfı oluşturunuz. Musteri parametresi alarak Musteri ekleme, listeleme,silme gibi metotları simule ediniz.
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "İrem";
+            musteri1.Soyadi = "Avcı";
+            musteri1.Bakiye = 1000;
+            musteri1.SonIslemTarihi = "12.12.2020";
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Halil";
+            musteri2.Soyadi = "İnal";
+            musteri2.Bakiye = 550;
+            musteri2.SonIslemTarihi = "01.09.2020";
+
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Selin";
+            musteri3.Soyadi = "Şahin";
+            musteri3.Bakiye = 0;
+            musteri3.SonIslemTarihi = "29.01.2021";
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(musteri.Bakiye);
+                Console.WriteLine(musteri.SonIslemTarihi);
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+
+            Console.WriteLine("---------------------------");
+
+            MusteriManager musteriManager1 = new MusteriManager();
+            musteriManager1.Sil(musteri1);
+            musteriManager1.Sil(musteri2);
+            musteriManager1.Sil(musteri3);
+        }
+    }
+}
